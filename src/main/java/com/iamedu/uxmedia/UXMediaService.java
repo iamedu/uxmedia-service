34e5a67f03a67588bd2179773773025cd3ad6f1e
Eduardo Diaz <iamedu@gmail.com>
2014-03-02 19:56:22 -0600

    Upgrading migrations

--- src/main/java/com/iamedu/uxmedia/UXMediaService.java
+++ src/main/java/com/iamedu/uxmedia/UXMediaService.java
@@ -1,9 +1,15 @@
 package com.iamedu.uxmedia;
 
 import com.yammer.dropwizard.Service;
+
+import com.yammer.dropwizard.assets.AssetsBundle;
+
+import com.yammer.dropwizard.db.DatabaseConfiguration;
+
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizard.assets.AssetsBundle;
+
+import com.yammer.dropwizard.migrations.MigrationsBundle;
 
 import com.bazaarvoice.dropwizard.assets.ConfiguredAssetsBundle;
 
@@ -16,11 +22,17 @@ public class UXMediaService extends Service<UXMediaConfiguration> {
     public void initialize(Bootstrap<UXMediaConfiguration> bootstrap) {
         bootstrap.setName("ux-wall");
         bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/app/", "/", "index.html"));
+        bootstrap.addBundle(new MigrationsBundle<UXMediaConfiguration>() {
+            @Override
+            public DatabaseConfiguration getDatabaseConfiguration(UXMediaConfiguration configuration) {
+                return configuration.getDatabaseConfiguration();
+            }
+        });
     }
 
     @Override
     public void run(UXMediaConfiguration configuration,
-                    Environment environment) {
+            Environment environment) {
     }
 
 }
