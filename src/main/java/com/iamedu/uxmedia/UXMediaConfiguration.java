46a46fd13c0356897e012a804edcdd5bfa8f50a4
Eduardo Diaz <iamedu@gmail.com>
2014-03-02 19:06:52 -0600

    Fixing service

--- src/main/java/com/iamedu/uxmedia/UXMediaConfiguration.java
+++ src/main/java/com/iamedu/uxmedia/UXMediaConfiguration.java
@@ -1,9 +1,34 @@
 package com.iamedu.uxmedia;
 
-import com.yammer.dropwizard.config.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.NotEmpty;
 
-public class UXMediaConfiguration extends Configuration {
+import com.yammer.dropwizard.db.DatabaseConfiguration;
+import com.yammer.dropwizard.config.Configuration;
 
+import com.bazaarvoice.dropwizard.assets.AssetsBundleConfiguration;
+import com.bazaarvoice.dropwizard.assets.AssetsConfiguration;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+
+public class UXMediaConfiguration extends Configuration implements AssetsBundleConfiguration {
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private final AssetsConfiguration assets = new AssetsConfiguration();
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DatabaseConfiguration database = new DatabaseConfiguration();
+
+    public DatabaseConfiguration getDatabaseConfiguration() {
+        return database;
+    }
+
+    @Override
+    public AssetsConfiguration getAssetsConfiguration() {
+        return assets;
+    }
 }
--- src/main/java/com/iamedu/uxmedia/UXMediaService.java
+++ src/main/java/com/iamedu/uxmedia/UXMediaService.java
@@ -3,6 +3,9 @@ package com.iamedu.uxmedia;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.assets.AssetsBundle;
+
+import com.bazaarvoice.dropwizard.assets.ConfiguredAssetsBundle;
 
 public class UXMediaService extends Service<UXMediaConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -12,6 +15,7 @@ public class UXMediaService extends Service<UXMediaConfiguration> {
     @Override
     public void initialize(Bootstrap<UXMediaConfiguration> bootstrap) {
         bootstrap.setName("ux-wall");
+        bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/app/", "/", "index.html"));
     }
 
     @Override
